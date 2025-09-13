9368f87fd9e49426576db99b81a8dce0b1a8e259
Lyrocs <cjuanole@gmail.com>
2025-09-13 17:01:20 +0200

    feat: add db pool to worker task

--- websockets-http2/src/main.rs
+++ websockets-http2/src/main.rs
@@ -79,7 +79,10 @@ impl AppState {
     }
 }
 
-async fn worker_task(mut command_receiver: mpsc::Receiver<Command>) {
+async fn worker_task(
+    mut command_receiver: mpsc::Receiver<Command>,
+    db_pool: SqlitePool,
+) {
     let mut counter: u32 = 0;
 
     while let Some(command) = command_receiver.recv().await {
@@ -87,6 +90,24 @@ async fn worker_task(mut command_receiver: mpsc::Receiver<Command>) {
             Command::Increment => {
                 counter += 1;
                 println!("Worker: Counter incremented to {}", counter);
+
+                let new_post = CreatePost {
+                    title: format!("Post {}", counter),
+                    content: format!("This is the content of post {}", counter),
+                };
+
+                let post = sqlx::query_as::<_, Post>(
+                    "INSERT INTO posts (title, content) VALUES (?, ?) RETURNING id, title, content",
+                )
+                .bind(new_post.title)
+                .bind(new_post.content)
+                .fetch_one(&db_pool)
+                .await;
+
+                match post {
+                    Ok(p) => println!("Worker: Created new post with id {}", p.id),
+                    Err(e) => eprintln!("Worker: Failed to create post: {}", e),
+                }
             }
             Command::GetCount { respond_to } => {
                 println!(
@@ -104,9 +125,10 @@ async fn main() {
     let (command_sender, command_receiver) = mpsc::channel(32);
     let (tx, _) = broadcast::channel(100);
 
-    tokio::spawn(worker_task(command_receiver));
-
     let app_state = AppState::new(command_sender, tx.clone()).await.unwrap();
+
+    let db_pool = app_state.db.clone();
+    tokio::spawn(worker_task(command_receiver, db_pool));
 
     tracing_subscriber::registry()
         .with(
